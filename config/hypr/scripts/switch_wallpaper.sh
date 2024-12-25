6f37a856f0d5befb636551fd709fa43befc541ca
NCastleSurvivor <1439345229@qq.com>
2024-12-25 16:16:03 +0800

    update

---
+++ config/hypr/scripts/switch_wallpaper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 壁纸存放的目录
+wallpaper_dir="$HOME/.config/hypr/wallpaper"
+
+# 获取目录中的所有图片文件
+wallpapers=("$wallpaper_dir"/*)
+
+# 随机选择一个图片文件
+random_wallpaper="${wallpapers[RANDOM % ${#wallpapers[@]}]}"
+
+# 使用swaybg设置壁纸
+swaybg -i "$random_wallpaper" -m fill
+

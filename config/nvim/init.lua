3e696b7509e60012f1a43012453dc41baaccca00
NCastleSurvivor <1439345229@qq.com>
2025-01-10 10:47:22 +0800

    update

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,8 @@
+vim.g.mapleader = "\\"
+vim.g.maplocalleader = ","
+
 require('config.options')
-require('config.plugins')
+require('config.lazy')
 require('config.keymaps')
 require('config.autocmds')
 require('config.colorscheme')
